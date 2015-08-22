7960461b25f10424d7572a46e3677dcce4174661
yaoqun.fyq <yaoqun.fyq@alibaba-inc.com>
2015-08-22 20:58:33 +0800

    读取png文件

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+    "image"
+    "log"
+    "os"
+    
+    _ "image/png"
+)
+
 func main() {
-    print("hello\n")
+    reader, err := os.Open("data/left.png")
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    img, _, err := image.Decode(reader)
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    if err := reader.Close(); err != nil {
+        log.Fatal(err)
+    }
+    
+    bound := img.Bounds()
+    print(bound.Min.X, " ", bound.Max.X, "\n")
+    print(bound.Min.Y, " ", bound.Max.Y, "\n")
+    r, g, b, _ := img.At(0, 0).RGBA()
+    print(r, " ", g, " ", b, "\n")
 }
